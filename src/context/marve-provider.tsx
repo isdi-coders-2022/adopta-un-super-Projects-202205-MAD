fc914779c6628741eb2ac21a3bd06a47bddd1090
Irene <iavivas3@gmail.com>
2022-06-06 12:18:34 +0200

    Add context and provider and characters model

---
+++ src/context/marve-provider.tsx
@@ -0,0 +1,31 @@
+import { ReactElement, useEffect, useState } from 'react';
+import { CharacterModel } from '../models/characters';
+import { MarvelApi } from '../services/marvelApi';
+import { MarvelContext } from './marvel-context';
+
+export function MarvelContextProvider({
+    children,
+}: {
+    children: ReactElement;
+}) {
+    const initialState: CharacterModel[] = [];
+    const [homePageCharacters, setHomePageCharacters] = useState(initialState);
+
+    useEffect(() => {
+        MarvelApi.getCharacters()
+            .then((resp) => resp)
+            .then((obj) => {
+                setHomePageCharacters(obj.data.results);
+            });
+    });
+
+    const context = {
+        homePageCharacters,
+    };
+
+    return (
+        <MarvelContext.Provider value={context}>
+            {children}
+        </MarvelContext.Provider>
+    );
+}
