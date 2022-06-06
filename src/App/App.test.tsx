b3cf62301c0d1f0b6b759769baa358154378d696
gomez-fernando <ferdgomez@hotmail.es>
2022-06-06 16:39:52 +0200

    Add test for context provider

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -4,6 +4,6 @@ import AppUI from './AppUI';
 
 test('renders learn react link', () => {
   render(<AppUI />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Marvel/i);
   expect(linkElement).toBeInTheDocument();
 });
