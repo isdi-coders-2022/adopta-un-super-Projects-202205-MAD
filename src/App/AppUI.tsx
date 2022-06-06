773f4c28aa98f9237467a9f8b61c8cae08763442
Pablo da Cunha <pablodacun@gmail.com>
2022-06-06 12:53:44 +0200

    renderApiList

--- src/App/AppUI.tsx
+++ src/App/AppUI.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { MarvelContext } from '../context/marvel-context';
 import '../styles/App.css';
 
 function AppUI() {
+    
+    const {homePageCharacters} = useContext(MarvelContext)
+
+
   return (
     <div className="App">
-      <h1>Hola!</h1>
+      <h1>Marvel</h1>
+
+      <ul>
+        {
+            homePageCharacters.map(item => (
+                <li key={item.name}>
+                    {item.name}
+                </li>
+            ))
+
+        }
+          
+      </ul>
     </div>
   );
 }
--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { MarvelContextProvider } from '../context/marvel-provider';
 import AppUI from "./AppUI"
 
 function App() {
   return (
+      <MarvelContextProvider>
       <AppUI/>
+      </MarvelContextProvider>
   );
 }
 
