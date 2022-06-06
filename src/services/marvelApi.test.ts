6a805acc504385d1f176df1b2e727902bfc9569c
Irene <iavivas3@gmail.com>
2022-06-06 16:03:19 +0200

    Add fetch testing of marvelApi

---
+++ src/services/marvelApi.test.ts
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+import { MarvelApi } from './marvelApi';
+
+describe('Given MarvelApi', () => {
+    describe('When we instantiate', () => {
+        describe('And we use method getCharacters', () => {
+            test('Then the fetch should return an array of 1 character', async () => {
+                global.fetch = jest.fn().mockResolvedValue({
+                    json: jest
+                        .fn()
+                        .mockResolvedValue([new MarvelApi(), new MarvelApi()]),
+                });
+                const result = await MarvelApi.getCharacters();
+                expect(fetch).toBeCalled();
+                expect(result.length).toBe(2);
+            });
+        });
+    });
+});
