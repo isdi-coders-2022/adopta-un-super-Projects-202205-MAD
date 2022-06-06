c01b9d8ea75d3a78c1f10400df46ddc285def77a
gomez-fernando <ferdgomez@hotmail.es>
2022-06-06 11:35:26 +0200

    Add module App and structure

---
+++ src/App/AppUI.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/App.css';
+
+function AppUI() {
+  return (
+    <div className="App">
+      <h1>Hola!</h1>
+    </div>
+  );
+}
+
+export default AppUI;
