9f215fe6c5e0eed9618d606e3fae3be5def52635
Irene <iavivas3@gmail.com>
2022-06-07 17:37:39 +0200

    Add some style in homePage

--- src/components/cardList/index.tsx
+++ src/components/cardList/index.tsx
@@ -7,9 +7,9 @@ export function CardList() {
     const { homePageCharacters } = useContext(MarvelContext);
 
     let template = (
-        <ul>
+        <ul className="card-list">
             {homePageCharacters.map((item) => (
-                <li key={item.name}>
+                <li key={item.name} className="card-list__card">
                     <Card superHero={item}></Card>
                 </li>
             ))}
