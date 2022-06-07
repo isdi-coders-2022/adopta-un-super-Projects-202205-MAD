54a08f45eaf3090eefafa77d5f79b716079c6c72
gomez-fernando <ferdgomez@hotmail.es>
2022-06-07 11:37:08 +0200

    Add new url construct method

--- src/services/marvelApi.ts
+++ src/services/marvelApi.ts
@@ -1,9 +1,20 @@
 import { CharacterModel } from "../models/characters";
 
+const url = 'http://gateway.marvel.com/v1/public/characters';
+const hash = '&ts=1000&apikey=51f17293928d718a3be05579990c29ed&hash=e5fe67d957b5f611358a74ecb6ab461c';
+
+
 export class MarvelApi {
-    static async getCharacters(): Promise<{data: {results: CharacterModel[]}}> {
+    // static async getCharacters(): Promise<{data: {results: CharacterModel[]}}> {
+    //     const result = await fetch(
+    //         `${url}?${hash}`
+    //     );
+    //     return await result.json();
+    // }
+
+    static async getCharacters(offset: string = '20'): Promise<{data: {results: CharacterModel[]}}> {
         const result = await fetch(
-            `http://gateway.marvel.com/v1/public/characters?ts=1000&apikey=51f17293928d718a3be05579990c29ed&hash=e5fe67d957b5f611358a74ecb6ab461c`
+            `${url}?limit=20&offset=${offset}${hash}`
         );
         return await result.json();
     }
